c8293d4fc8f5714e95105f2076153b92b032a316
Philipp Fritsche <ph.fritsche@gmail.com>
2023-01-02 16:48:40 +0000

    test: work around race condition with event handler

--- tests/environment/select.ts
+++ tests/environment/select.ts
@@ -1,4 +1,5 @@
 import {isJsdomEnv, render} from '#testHelpers'
+import DTL from '#src/_interop/dtl'
 
 test('`Selection.setBaseAndExtent()` resets input selection in browser', async () => {
   const {element} = render<HTMLInputElement>(`<input value="foo"/>`, {
@@ -9,4 +10,16 @@ test('`Selection.setBaseAndExtent()` resets input selection in browser', async (
   element.ownerDocument.getSelection()?.setBaseAndExtent(element, 0, element, 0)
 
   expect(element.selectionStart).toBe(isJsdomEnv() ? 3 : 0)
+})
+
+test('events are not guaranteed to be dispatched on same microtask in browser', async () => {
+  const {element} = render<HTMLInputElement>(`<input value="foo"/>`)
+  const onSelect = mocks.fn()
+  element.addEventListener('select', onSelect)
+
+  element.setSelectionRange(1, 2)
+
+  expect(onSelect).toBeCalledTimes(isJsdomEnv() ? 1 : 0)
+
+  await DTL.waitFor(() => expect(onSelect).toBeCalledTimes(1))
 })
